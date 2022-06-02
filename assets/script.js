f80bbb576cfb99af54f6450216d4e560a96cc5f5
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-06-02 09:47:45 -0700

    adding base styles and getting js started

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 var apiKey = "cbbe6c2ed3050fe64ee8a01d85f6dcac"
+
+//DOM ELEMENTS
+var search = document.getElementById("searchButton");
+
+//DECLARED FUNCTIONS
+
+function searchFor() {
+    console.log(hello);
+}
+
+
+
+//EVENT LISTENERS
+search.addEventListener("click", searchFor);

1aee249172a5be861d50663e4ed7a282bd4c097e
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-06-02 09:56:08 -0700

    added dom elements to js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,12 @@
+//GLOBAL VARIABLES
 var apiKey = "cbbe6c2ed3050fe64ee8a01d85f6dcac"
 
 //DOM ELEMENTS
-var search = document.getElementById("searchButton");
+var searchBtn = document.getElementById("searchButton");
+var historyEl = document.getElementById("searchHistory");
+var currentEl = document.getElementById("current");
+var forecastEL = document.getElementById("forecast");
+
 
 //DECLARED FUNCTIONS
 

eae966b6be83b92903d5cacf910968f925bba9e9
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-06-02 10:48:34 -0700

    connected api successfully

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var apiKey = "cbbe6c2ed3050fe64ee8a01d85f6dcac"
 
 //DOM ELEMENTS
 var searchBtn = document.getElementById("searchButton");
+var cityEl = document.getElementById("newCity");
 var historyEl = document.getElementById("searchHistory");
 var currentEl = document.getElementById("current");
 var forecastEL = document.getElementById("forecast");
@@ -10,11 +11,25 @@ var forecastEL = document.getElementById("forecast");
 
 //DECLARED FUNCTIONS
 
-function searchFor() {
-    console.log(hello);
+// function citySubmitHandler() {
+//     preventDefault();
+//     var city =  cityEl.value.trim();
+//     console.log(city);
+//     if (city) {
+//         getWeather(city);
+//     } else {
+//         alert("City not found. Please enter a different City");
+//     }
+// };
+
+function getWeather() {
+    var city =  cityEl.value.trim();
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    fetch(queryURL);
+    console.log(queryURL);
 }
 
 
 
 //EVENT LISTENERS
-search.addEventListener("click", searchFor);
+searchBtn.addEventListener("click", getWeather);
